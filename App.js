e10b7c68be495de1e429cd676bf1b1da632bf65a
Aulianyb <18221066@std.stei.itb.ac.id>
2024-03-04 22:08:11 +0700

    feat : splash screen
    - Added splash screen
    - Added placeholder login Screen

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Welcome To</Text>
+      <Text style={styles.subtitle}>Unwind</Text>
+      <TextInput 
+        style={
+          {height: 50, 
+          borderRadius:10,
+          padding:10,
+          margin:10,
+          backgroundColor:'#e3dfe6',
+          width : 300,
+          fontSize:18
+        }}
+        placeholder="Username"
+        onChangeText={newUsername => setUsername(newUsername)}
+        defaultValue={username}/>
+            <TextInput 
+        style={{
+          height: 50, 
+          borderRadius:10,
+          padding:10,
+          margin:10,
+          backgroundColor:'#e3dfe6',
+          width : 300,
+          fontSize:18
+        }}
+        placeholder="Password"
+        onChangeText={newPassword => setPassword(newPassword)}
+        defaultValue={password}
+        secureTextEntry={true}/> 
+      <Button title="login"
+      color='#9768CD'
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +48,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F8F7F3',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo : {
+    height: 200,
+    width:200
+  },
+  title : {
+    fontSize: 24,
+    color : '#9768CD', 
+  }, 
+  subtitle : {
+    fontSize: 36,
+    color : '#9768CD',
+    fontWeight : 'bold', 
+    marginBottom : 30 
+  }
 });
