5155400d272f757cd895d022296f87332609f1e2
Aulianyb <18221066@std.stei.itb.ac.id>
2024-05-23 10:20:47 +0700

    fix : article scroll feature

---
+++ src/app/components/appStyles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    navbarPosition: {
+        position: 'absolute',
+        bottom: 0,
+        width: '100%',
+      },
+});
